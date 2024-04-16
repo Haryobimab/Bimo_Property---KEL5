21c282b1c16fbd13726559ab9a7b9a33364bcf4b
devitaputrii <116071679+devitaputrii@users.noreply.github.com>
2024-04-16 20:52:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\daftar_ulasan;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UlasanController;
