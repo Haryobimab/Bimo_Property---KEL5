487ec6e25131c362d5b9b88eda84749f7b81347b
Haryo Bima Bodrokusumo <91707459+Haryobimab@users.noreply.github.com>
2024-05-09 11:17:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Auth\SocialiteController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\daftar_ulasan;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UlasanController;
 use App\Http\Controllers\AdminController;
